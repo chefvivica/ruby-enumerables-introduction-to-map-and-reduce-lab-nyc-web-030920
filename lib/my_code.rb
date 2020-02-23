b04374052e2bc79aefb98db071a7f86c698fc7f3
Vivica Ma <vivicacongma@gmail.com>
2020-02-23 15:19:11 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -20,7 +20,7 @@ def map_to_negativize(source_array)
   i = 0 
   
     while i < source_array.length do 
-      source_array[i].to_i =  -(source_array[i])
+      source_array[i] = - source_array[i]
       i += 1 
     end 
   source_array
