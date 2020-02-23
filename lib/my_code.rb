49cc3e1f984c776e2a64a54160dbf791f3a9a29e
Vivica Ma <vivicacongma@gmail.com>
2020-02-23 16:00:48 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -76,3 +76,12 @@ def reduce_to_all_true(source_array)
   true 
 end 
 
+def reduce_to_any_true(source_array)
+    i = 0 
+      while i < source_array.length do 
+        if source_array[i]===true 
+            return false
+          end 
+         i += 1 
+        end 
+      true   
