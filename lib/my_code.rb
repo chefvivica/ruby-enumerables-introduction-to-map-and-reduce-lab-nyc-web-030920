63d8280b980d7216200ec9ac8b1c0979007d56d2
Vivica Ma <vivicacongma@gmail.com>
2020-02-23 15:36:00 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -57,7 +57,7 @@ def reduce_to_total(source_array, starting_point)
     starting_point = 0 
     total = 0 
       while i < source_array.length do 
-        total = source_array[starting_point] += 
+        total += source_array[starting_point] 
         starting_point += 1 
       end 
  total
