2824d659c7f236a0899380b9df987474351a1f81
Vivica Ma <vivicacongma@gmail.com>
2020-02-23 15:23:39 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -38,7 +38,7 @@ end
 def map_to_double(source_array)
     i = 0 
     while i < source_array.length do 
-      source_array[i] = source_array[i]**
+      source_array[i] = source_array[i] * 2
       i += 1 
     end 
   source_array
