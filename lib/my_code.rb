49f2ca3c94c9e37480a40ddf69f955d05aa45f06
Vivica Ma <vivicacongma@gmail.com>
2020-02-23 15:38:28 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -63,3 +63,12 @@ def reduce_to_total(source_array, starting_point)
  total
 end 
   
+  
+def reduce_to_all_true(source_array)
+   i = 0 
+    while i < source_array.length do 
+      source_array[i] == true 
+      i += 1 
+    end 
+  source_array
+end 
