5b97123aa2388d275b74eec2886ab2b1d3e4c175
Vivica Ma <vivicacongma@gmail.com>
2020-02-23 15:08:15 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,16 @@
 # My Code here....
+
+# ### `map`-like
+
+# - `map_to_negativize(source_array)`
+# - `map_to_no_change(source_array)`
+# - `map_to_double(source_array)`
+# - `map_to_square(source_array)`
+
+# Remember, all `map` methods return a new `Array`.
+
+# ### `reduce`-like
+
+# - `reduce_to_total(source_array, starting_point)`
+# - `reduce_to_all_true(source_array)`
+# - `reduce_to_any_true(source_array)`
